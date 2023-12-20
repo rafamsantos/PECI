fb363d6097c92b0d75306c854840c892db374f9a
rmlameiras@ua.pt <rmlameiras@ua.pt>
2023-12-20 16:39:20 +0000

    Door started

--- door.py
+++ door.py
@@ -1,3 +1,10 @@
+import machine
+from machine import Pin, SPI, PWM, I2C
+from nfc import NFC
+import utime, time
+import st7789
+import vga1_bold_16x32 as font
+import os
 import socket
 from cmath import sqrt
 import os
@@ -18,25 +25,64 @@ import random
 import rsa
 from Crypto.Cipher import AES
 from cryptography.hazmat.primitives import padding
-import bingogame
 import hashlib
+import ndef
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-from parea import gameFlag
 
+backend = default_backend()
+
+def set_asymetric(): 
+	priv = RSA.generate(2048)
+	pub = priv.publickey()
+
+	return priv, pub
+
+def main():
 # Create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Get the server's hostname and port
-server_host = socket.gethostname()
-server_port = 9999
+    server_host = socket.gethostname()
+    server_port = 9999
 
 # Connect to the server
-client_socket.connect((server_host, server_port))
+    client_sock.connect((server_host, server_port))
 
 # Receive data from the server
-response = client_socket.recv(1024)
-print(f"Server says: {response.decode()}")
+    response = client_sock.recv(1024)
+    print(f"Server says: {response.decode()}")
+    user = "1"
+    client_priv, client_pub = set_asymetric()	
+	# d = client_pub.verify(user, j)	
+    client_sock.send(client_pub.exportKey(format='PEM', passphrase=None, pkcs=1))
+    server_pub = RSA.importKey(client_sock.recv(2048), passphrase=None)
+    #print(server_pub)
+    message = "I'm a door"
+    client_sock.send(message.encode())
+    st = recv_dict(client_sock)
+    key = base64.b64decode(st["cipher"])
+    iv = base64.b64decode(st["iv"])
+    ud = hashlib.sha256(str(key).encode("utf8")).digest()
+    u = server_pub.verify(ud, st["sig"])
+    print(u)
+    if u == False:
+        print("The conction was compromise. We are disconcting you for your safety\n")
+        client_sock.close()
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend)
+    ud = hashlib.sha256(str(key).encode("utf8")).digest()
+    encryptor = cipher.encryptor()
+    decryptor = cipher.decryptor()
+    i_m = st["You are client"]
+    while True:
+        print("wating...\n")
+        
+        
+        
+        
+    
+        
+    
 
-# Close the connection
-client_socket.close()
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -30,11 +30,22 @@ import string
 # Function to handle each client's connection
 backend = default_backend()
 many_users = 0
+many_doors = 0
 
 def databasecreate_user(db):
     db = sql.connect("mock_database.db")
     c = db.cursor()
     c.execute("""CREATE TABLE userdata(ID TEXT, PUBLIC_KEY TEXT, SYMETRIC_KEY TEXT, IV TEXT)""")
+    db.commit()
+    db.close()
+    
+    return None
+
+
+def databasecreate_door(db):
+    db = sql.connect("mock_database.db")
+    c = db.cursor()
+    c.execute("""CREATE TABLE doordata(ID TEXT, PUBLIC_KEY TEXT, SYMETRIC_KEY TEXT, IV TEXT)""")
     db.commit()
     db.close()
     
@@ -68,6 +79,16 @@ def insertdatabase_client(key, iv, client_pub):
     db = sql.connect("mock_database.db")
     c = db.cursor()
     c.execute("INSERT INTO userdata VALUES (?, ?, ?, ?)", (str(many_users), str(key), str(iv), str(client_pub)))
+    db.commit()
+    
+    db.close()
+
+
+def insertdatabase_door(key, iv, client_pub):
+    many_doors = 1
+    db = sql.connect("mock_database.db")
+    c = db.cursor()
+    c.execute("INSERT INTO doordata VALUES (?, ?, ?, ?)", (str(many_doors), str(key), str(iv), str(client_pub)))
     db.commit()
     
     db.close()
@@ -118,20 +139,20 @@ def handle_client(client_sock, addr):
         decryptor = cipher.decryptor()
         if response.decode() == "I'm a cliente":
             insertdatabase_client(key, iv, client_pub)
-        while True:
-            request = recv_dict(client_sock)
-            the_id_given = request["I'm"]
-            ud = hashlib.sha256(str(the_id_given).encode("utf8")).digest()
-            u = client_pub.verify(ud, request["Sig"])
-            if u != True:
-                st = {"Compromise": "You where compromise\nQuiting..."}
-                send_dict(client_sock, st)
-                client_sock.close() 
-            elif request["command"] == "NFC":
-                data = ''.join(random.choices(string.ascii_letters + string.digits, k=15))
+            while True:
+                request = recv_dict(client_sock)
+                the_id_given = request["I'm"]
+                ud = hashlib.sha256(str(the_id_given).encode("utf8")).digest()
+                u = client_pub.verify(ud, request["Sig"])
+                if u != True:
+                    st = {"Compromise": "You where compromise\nQuiting..."}
+                    send_dict(client_sock, st)
+                    client_sock.close() 
+                elif request["command"] == "NFC":
+                    data = ''.join(random.choices(string.ascii_letters + string.digits, k=15))
 
 # Create an NDEF Text Record
-                data = data.zfill(16)
+                    data = data.zfill(16)
                 #record = ndef.TextRecord(data)
 
 # Encode the record
@@ -142,16 +163,18 @@ def handle_client(client_sock, addr):
 
 # Convert bytes to hexadecimal string
                 #hex_string = binascii.hexlify(encoded_message).decode()
-                insertdatabase_NFC(str(data), str(request["I'm"]))
-                record = encryptor.update(str(data).encode("utf-8")) + encryptor.finalize()
-                st = {"NFC code": str(base64.b64encode (record), "utf8")}
-                send_dict(client_sock, st)
-            elif request["command"] == "LOG":
-                to_sent = read_log(request["User"])
-                record = encryptor.update(str(to_sent).encode("utf-8")) + encryptor.finalize()
-                st = {"The Logs": str(base64.b64encode (record), "utf8")}
-                send_dict(to_sent)
-                
+                    insertdatabase_NFC(str(data), str(request["I'm"]))
+                    record = encryptor.update(str(data).encode("utf-8")) + encryptor.finalize()
+                    st = {"NFC code": str(base64.b64encode (record), "utf8")}
+                    send_dict(client_sock, st)
+                elif request["command"] == "LOG":
+                    to_sent = read_log(request["User"])
+                    record = encryptor.update(str(to_sent).encode("utf-8")) + encryptor.finalize()
+                    st = {"The Logs": str(base64.b64encode (record), "utf8")}
+                    send_dict(to_sent)
+        else:
+            insertdatabase_door(key, iv, client_pub)  
+            #Para ser feito... 
                 
 
                 
@@ -217,6 +240,19 @@ def main():
             databasecreate_log(db3)
             print(0)
             db3.close()
+    
+    db4 = sql.connect("mock_database.db")
+    c = db4.cursor()
+    try:
+            a = c.execute("SELECT * FROM doordata")
+            print(9)
+            for row in a:
+                print(row)
+            db4.close()
+    except(sql.Error):
+            databasecreate_door(db4)
+            print(0)
+            db4.close()
 
 # Bind to the port
     server_socket.bind((host, port))
